7a37c5712f9e23168ef8892af26879fc298fc042
Cikaros <Cikaros@qq.com>
2025-04-08 17:33:15 +0800

    fix JaNetfilter

--- ja-netfilter.rb
+++ ja-netfilter.rb
@@ -10,44 +10,6 @@ class JaNetfilter < Formula
 
   def install
     prefix.install Dir["*"]
-
-    # Create a plist file for the service
-    (libexec/"ja-netfilter.plist").write <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>jetbrains.vmoptions</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>sh</string>
-          <string>-c</string>
-          <string>
-            launchctl setenv "IDEA_VM_OPTIONS" "#{opt_prefix}/vmoptions/idea.vmoptions";
-            launchctl setenv "CLION_VM_OPTIONS" "#{opt_prefix}/vmoptions/clion.vmoptions";
-            launchctl setenv "PHPSTORM_VM_OPTIONS" "#{opt_prefix}/vmoptions/phpstorm.vmoptions";
-            launchctl setenv "GOLAND_VM_OPTIONS" "#{opt_prefix}/vmoptions/goland.vmoptions";
-            launchctl setenv "PYCHARM_VM_OPTIONS" "#{opt_prefix}/vmoptions/pycharm.vmoptions";
-            launchctl setenv "WEBSTORM_VM_OPTIONS" "#{opt_prefix}/vmoptions/webstorm.vmoptions";
-            launchctl setenv "WEBIDE_VM_OPTIONS" "#{opt_prefix}/vmoptions/webide.vmoptions";
-            launchctl setenv "RIDER_VM_OPTIONS" "#{opt_prefix}/vmoptions/rider.vmoptions";
-            launchctl setenv "DATAGRIP_VM_OPTIONS" "#{opt_prefix}/vmoptions/datagrip.vmoptions";
-            launchctl setenv "RUBYMINE_VM_OPTIONS" "#{opt_prefix}/vmoptions/rubymine.vmoptions";
-            launchctl setenv "APPCODE_VM_OPTIONS" "#{opt_prefix}/vmoptions/appcode.vmoptions";
-            launchctl setenv "DATASPELL_VM_OPTIONS" "#{opt_prefix}/vmoptions/dataspell.vmoptions";
-            launchctl setenv "GATEWAY_VM_OPTIONS" "#{opt_prefix}/vmoptions/gateway.vmoptions";
-            launchctl setenv "JETBRAINS_CLIENT_VM_OPTIONS" "#{opt_prefix}/vmoptions/jetbrains_client.vmoptions";
-            launchctl setenv "JETBRAINSCLIENT_VM_OPTIONS" "#{opt_prefix}/vmoptions/jetbrainsclient.vmoptions";
-            launchctl setenv "STUDIO_VM_OPTIONS" "#{opt_prefix}/vmoptions/studio.vmoptions";
-            launchctl setenv "DEVECOSTUDIO_VM_OPTIONS" "#{opt_prefix}/vmoptions/devecostudio.vmoptions";
-          </string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-      </dict>
-      </plist>
-    EOS
   end
 
   def post_install
@@ -69,19 +31,32 @@ class JaNetfilter < Formula
       ohai "VM options directory not found: #{vmoptions_dir}"
     end
 
-    # Install the service plist file
-    system "cp", "#{libexec}/ja-netfilter.plist", "/Library/LaunchDaemons/jetbrains.vmoptions.plist"
-    system "chmod", "644", "/Library/LaunchDaemons/jetbrains.vmoptions.plist"
-    system "chown", "root:wheel", "/Library/LaunchDaemons/jetbrains.vmoptions.plist"
-
-    # Load the service
-    system "launchctl", "load", "-w", "/Library/LaunchDaemons/jetbrains.vmoptions.plist"
   end
 
-  def uninstall
-    # Uninstall the service plist file
-    system "launchctl", "unload", "/Library/LaunchDaemons/jetbrains.vmoptions.plist"
-    system "rm", "/Library/LaunchDaemons/jetbrains.vmoptions.plist"
+  service do
+    run ["/bin/sh", "-c",
+         %Q[
+           launchctl setenv "IDEA_VM_OPTIONS" "#{opt_prefix}/vmoptions/idea.vmoptions";
+           launchctl setenv "CLION_VM_OPTIONS" "#{opt_prefix}/vmoptions/clion.vmoptions";
+           launchctl setenv "PHPSTORM_VM_OPTIONS" "#{opt_prefix}/vmoptions/phpstorm.vmoptions";
+           launchctl setenv "GOLAND_VM_OPTIONS" "#{opt_prefix}/vmoptions/goland.vmoptions";
+           launchctl setenv "PYCHARM_VM_OPTIONS" "#{opt_prefix}/vmoptions/pycharm.vmoptions";
+           launchctl setenv "WEBSTORM_VM_OPTIONS" "#{opt_prefix}/vmoptions/webstorm.vmoptions";
+           launchctl setenv "WEBIDE_VM_OPTIONS" "#{opt_prefix}/vmoptions/webide.vmoptions";
+           launchctl setenv "RIDER_VM_OPTIONS" "#{opt_prefix}/vmoptions/rider.vmoptions";
+           launchctl setenv "DATAGRIP_VM_OPTIONS" "#{opt_prefix}/vmoptions/datagrip.vmoptions";
+           launchctl setenv "RUBYMINE_VM_OPTIONS" "#{opt_prefix}/vmoptions/rubymine.vmoptions";
+           launchctl setenv "APPCODE_VM_OPTIONS" "#{opt_prefix}/vmoptions/appcode.vmoptions";
+           launchctl setenv "DATASPELL_VM_OPTIONS" "#{opt_prefix}/vmoptions/dataspell.vmoptions";
+           launchctl setenv "GATEWAY_VM_OPTIONS" "#{opt_prefix}/vmoptions/gateway.vmoptions";
+           launchctl setenv "JETBRAINS_CLIENT_VM_OPTIONS" "#{opt_prefix}/vmoptions/jetbrains_client.vmoptions";
+           launchctl setenv "JETBRAINSCLIENT_VM_OPTIONS" "#{opt_prefix}/vmoptions/jetbrainsclient.vmoptions";
+           launchctl setenv "STUDIO_VM_OPTIONS" "#{opt_prefix}/vmoptions/studio.vmoptions";
+           launchctl setenv "DEVECOSTUDIO_VM_OPTIONS" "#{opt_prefix}/vmoptions/devecostudio.vmoptions";
+         ]]
+    keep_alive true
+    log_path var/"log/ja-netfilter.log"
+    error_log_path var/"log/ja-netfilter.err.log"
   end
 
 end
