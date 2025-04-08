27f2a3e70a86b544441b28a0aa3f3271578e1ea8
Cikaros <Cikaros@qq.com>
2025-04-08 15:38:57 +0800

    add clion & datagrip

---
+++ Casks/clion.rb
@@ -0,0 +1,37 @@
+cask "clion" do
+  arch arm: "-aarch64"
+
+  version "2024.1.6,241.19416.21"
+
+  url "https://download.jetbrains.com/cpp/CLion-#{version.csv.first}#{arch}.dmg"
+  name "CLion"
+  desc "C and C++ IDE"
+  homepage "https://www.jetbrains.com/clion/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=CL&latest=true&type=release"
+    strategy :json do |json|
+      json["CL"]&.map do |release|
+        version = release["version"]
+        build = release["build"]
+        next if version.blank? || build.blank?
+
+        "#{version},#{build}"
+      end
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "CLion.app"
+  binary "#{appdir}/CLion.app/Contents/MacOS/clion"
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/CLion#{version.major_minor}",
+    "~/Library/Caches/JetBrains/CLion#{version.major_minor}",
+    "~/Library/Logs/JetBrains/CLion#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.CLion.plist",
+    "~/Library/Saved Application State/com.jetbrains.CLion.savedState",
+  ]
+end
---
+++ Casks/datagrip.rb
@@ -0,0 +1,36 @@
+cask "datagrip" do
+  arch arm: "-aarch64"
+
+  version "22024.1.5,241.19072.24"
+
+  url "https://download.jetbrains.com/datagrip/datagrip-#{version.csv.first}#{arch}.dmg"
+  name "DataGrip"
+  desc "Databases and SQL IDE"
+  homepage "https://www.jetbrains.com/datagrip/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=DG&latest=true&type=release"
+    strategy :json do |json|
+      json["DG"]&.map do |release|
+        version = release["version"]
+        build = release["build"]
+        next if version.blank? || build.blank?
+
+        "#{version},#{build}"
+      end
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "DataGrip.app"
+  binary "#{appdir}/DataGrip.app/Contents/MacOS/datagrip"
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/DataGrip*",
+    "~/Library/Caches/JetBrains/DataGrip*",
+    "~/Library/Logs/JetBrains/DataGrip*",
+    "~/Library/Saved Application State/com.jetbrains.datagrip.savedState",
+  ]
+end
