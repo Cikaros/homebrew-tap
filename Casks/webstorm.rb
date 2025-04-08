7e753bad1f2b63239b6f00b6de83f45bec23bbff
Cikaros <Cikaros@qq.com>
2025-04-08 15:46:32 +0800

    fix goland & add webstorm

---
+++ Casks/webstorm.rb
@@ -0,0 +1,41 @@
+cask "webstorm" do
+  arch arm: "-aarch64"
+
+  version "2024.1.7,241.19416.2"
+
+  url "https://download.jetbrains.com/webstorm/WebStorm-#{version.csv.first}#{arch}.dmg"
+  name "WebStorm"
+  desc "JavaScript IDE"
+  homepage "https://www.jetbrains.com/webstorm/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=WS&latest=true&type=release"
+    strategy :json do |json|
+      json["WS"]&.map do |release|
+        version = release["version"]
+        build = release["build"]
+        next if version.blank? || build.blank?
+
+        "#{version},#{build}"
+      end
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "WebStorm.app"
+  binary "#{appdir}/WebStorm.app/Contents/MacOS/webstorm"
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/WebStorm#{version.major_minor}",
+    "~/Library/Caches/com.apple.nsurlsessiond/Downloads/com.jetbrains.WebStorm",
+    "~/Library/Caches/JetBrains/WebStorm#{version.major_minor}",
+    "~/Library/Logs/JetBrains/WebStorm#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.WebStorm.plist",
+    "~/Library/Preferences/jetbrains.webstorm.*.plist",
+    "~/Library/Preferences/WebStorm#{version.major_minor}",
+    "~/Library/Preferences/webstorm.plist",
+    "~/Library/Saved Application State/com.jetbrains.WebStorm.savedState",
+  ]
+end
