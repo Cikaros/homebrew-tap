c94edbd73bc0fb5d286735dec4e6e280ae1af995
Cikaros <Cikaros@qq.com>
2025-04-08 15:45:01 +0800

    fix intellij-idea & add goland

---
+++ Casks/goland.rb
@@ -0,0 +1,39 @@
+cask "goland" do
+  arch arm: "-aarch64"
+
+  version "2024.1.6,241.19072.18"
+
+  url "https://download.jetbrains.com/go/goland-#{version.csv.first}#{arch}.dmg"
+  name "Goland"
+  desc "Go (golang) IDE"
+  homepage "https://www.jetbrains.com/go/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=GO&latest=true&type=release"
+    strategy :json do |json|
+      json["GO"]&.map do |release|
+        version = release["version"]
+        build = release["build"]
+        next if version.blank? || build.blank?
+
+        "#{version},#{build}"
+      end
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "GoLand.app"
+  binary "#{appdir}/GoLand.app/Contents/MacOS/goland"
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/GoLand",
+    "~/Library/Application Support/JetBrains/GoLand#{version.major_minor}",
+    "~/Library/Caches/JetBrains/GoLand#{version.major_minor}",
+    "~/Library/Logs/JetBrains/GoLand#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.goland.plist",
+    "~/Library/Preferences/GoLand#{version.major_minor}",
+    "~/Library/Saved Application State/com.jetbrains.goland.SavedState",
+  ]
+end
