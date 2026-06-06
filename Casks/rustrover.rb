224e54b43e946f792e0e78aa20dc09d355f5a8b1
Cikaros <46598867+Cikaros@users.noreply.github.com>
2026-06-06 22:02:31 +0800

    Create cask for RustRover IDE
    Add cask for RustRover IDE with version and dependencies.

---
+++ Casks/rustrover.rb
@@ -0,0 +1,46 @@
+cask "rustrover" do
+  arch arm: "-aarch64"
+
+  version "2025.3.6,253.33514.21"
+
+  url "https://download.jetbrains.com/rustrover/RustRover-#{version.csv.first}#{arch}.dmg"
+  name "RustRover"
+  desc "Rust IDE"
+  homepage "https://www.jetbrains.com/rust/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=RR&latest=true&type=release"
+    strategy :json do |json|
+      json["RR"]&.map do |release|
+        version = release["version"]
+        build = release["build"]
+        next if version.blank? || build.blank?
+
+        "#{version},#{build}"
+      end
+    end
+  end
+
+  auto_updates true
+  depends_on :macos
+
+  app "RustRover.app"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/rustrover.wrapper.sh"
+  binary shimscript, target: "rustrover"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{appdir}/RustRover.app/Contents/MacOS/rustrover' "$@"
+    EOS
+  end
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/RustRover#{version.major_minor}",
+    "~/Library/Caches/JetBrains/RustRover#{version.major_minor}",
+    "~/Library/Logs/JetBrains/RustRover#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.rustrover.plist",
+    "~/Library/Saved Application State/com.jetbrains.rustrover.savedState",
+  ]
+end
